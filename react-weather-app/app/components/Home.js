666762cde0e315dc46ea9acacdb62d3ecb9c711e
Martin Rodin <rodin.martin@gmail.com>
2018-06-14 20:35:20 +0200

    add input and button

--- react-weather-app/app/components/Home.js
+++ react-weather-app/app/components/Home.js
@@ -3,8 +3,14 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <div className="home-wrapper">
-        <div>Return Home</div>
+      <div className='home-wrapper'>
+        <div className='form-wrapper'>
+          <div className='hint'>Enter a City and State</div>
+          <form>
+            <input type='text' />
+            <button>Get Weather</button>
+          </form>
+        </div>
       </div>
     )
   }
