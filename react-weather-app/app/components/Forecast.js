d1cb2194184d14cb8ff6275bb9b73a92aa7bcc43
Martin Rodin <rodin.martin@gmail.com>
2018-07-27 19:49:47 +0200

    add Loader and Forecast components

--- react-weather-app/app/components/Forecast.js
+++ react-weather-app/app/components/Forecast.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import queryString from 'query-string';
+import Loader from './Loader';
+import { getCurrentWeather } from '../utils/api';
 
-export default function Forecast() {
-  return (
-    <p>FORECAST</p>
-  )
+class Forecast extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      forecast: null
+    }
+
+    this.getForecast = this.getForecast.bind(this);
+  }
+
+  componentDidMount() {
+    const { address } = queryString.parse(this.props.location.search);
+
+    this.getForecast(address);
+  }
+
+  async getForecast(address) {
+    // reset forecast to null before fetching new data
+    this.setState(() => { return { forecast: null } });
+
+    const forecast = await getCurrentWeather(address);
+    this.setState(() => { return { forecast: forecast } });
+  }
+
+  render() {
+    const { forecast } = this.state;
+
+    return (
+      <div>
+        {!forecast ? <Loader /> : <p>Loading finished.</p>}
+      </div>
+    )
+  }
 }
+
+export default Forecast;
--- react-weather-app/app/components/Navigation.js
+++ react-weather-app/app/components/Navigation.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import AddressForm from './AddressForm';
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
     <nav className='nav row'>
       <div className='nav__title'>
-        <a href="#" className='nav__link'>React Weather App</a>
+        <Link
+          className='nav__link'
+          to={{
+            pathname: '/'
+          }}>
+          React Weather App
+        </Link>
       </div>
       <AddressForm direction={'row'} />
     </nav>
